26398362cf455bc99ce81b8b89a1be21567893bb
Galina Zbitneva <galinaknurenko@gmail.com>
2025-05-13 17:07:21 +0300

    creating spring boot project

---
+++ src/main/java/my/springProjects/NorrisJokes/NorrisJokesApplication.java
@@ -0,0 +1,13 @@
+package my.springProjects.NorrisJokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NorrisJokesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NorrisJokesApplication.class, args);
+	}
+
+}
